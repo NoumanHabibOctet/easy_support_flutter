b0071869522dfdc73e5ab31034b005feb388fe46
NoumanHabibOctet <nouman@octetsolutions.io>
2026-02-17 12:08:18 +0500

    working on ui

--- lib/src/widgets/easy_support_hero_section.dart
+++ lib/src/widgets/easy_support_hero_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'easy_support_animated_chat_icon.dart';
 import 'easy_support_color_utils.dart';
 
 class EasySupportHeroSection extends StatelessWidget {
@@ -21,26 +22,8 @@ class EasySupportHeroSection extends StatelessWidget {
 
     return Column(
       children: [
-        Container(
-          width: 122,
-          height: 122,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.white,
-            border: Border.all(color: iconBorderColor),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.08),
-                blurRadius: 22,
-                offset: const Offset(0, 10),
-              ),
-            ],
-          ),
-          child: const Icon(
-            Icons.chat_bubble_outline_rounded,
-            size: 62,
-            color: Color(0xFF111827),
-          ),
+        EasySupportAnimatedChatIcon(
+          borderColor: iconBorderColor,
         ),
         const SizedBox(height: 24),
         Text(
